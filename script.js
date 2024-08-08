829c5e9632d5979e3915e2878090eebdde09b662
William Zhang <zhangywu@umich.edu>
2024-08-08 01:51:11 -0400

    images done

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", writeLoop);
 
 // Image transition
 
-const images = ["image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg", "image.jpeg"];
+const images = ["image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg", "image.jpg"];
 let currentImageIndex = 0;
 
 function changeImage() {
